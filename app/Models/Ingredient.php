df706ef94cdb52d62cdd3fafa9c0104bd680a1d2
Jad <jad.khoury14@gmail.com>
2021-06-11 21:59:09 +0300

    Created 'Ingredient' model

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use App\Base\BaseModel;
+
+class Ingredient extends BaseModel
+{
+    /**
+     * Set hidden field
+     *
+     * @var array
+     */
+    protected $hidden = ['measure'];
+
+    /**
+     * Eager load relations
+     *
+     * @var array
+     */
+    protected $with = ['measure'];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['measurement_value'];
+
+    /**
+     * Set the fields that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'measure_id'
+    ];
+
+    /**
+     * Get the measurement value associated with the Ingredient
+     *
+     * @return string
+     */
+    public function getMeasurementValueAttribute()
+    {
+        return isset($this->measure) ? $this->measure->name : '';
+    }
+
+
+    /**
+     * Get the measure record associated with the ingredient
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function measure()
+    {
+        return $this->belongsTo(Measure::class, 'measure_id');
+    }
+}
