373a31f908d6fcd3815385c8083272bb53a18c42
Jad <jad.khoury14@gmail.com>
2021-06-12 16:18:54 +0300

    Added logic for 'add' and 'get' methods in 'Ingredient' controller

--- app/Http/Controllers/Api/IngredientController.php
+++ app/Http/Controllers/Api/IngredientController.php
@@ -6,18 +6,24 @@ namespace App\Http\Controllers\Api;
 use App\Base\BaseController;
 use App\Http\Requests\AddIngredientRequest;
 use App\Http\Requests\GetRequiredIngredients;
+use App\Models\Ingredient;
 
 class IngredientController extends BaseController
 {
 
     public function add(AddIngredientRequest $request)
     {
+        $ingredient = Ingredient::query()->create($request->validated());
 
+        return $this->response->statusOk([
+            'message'    => 'Ingredient added successfully',
+            'ingredient' => $ingredient
+        ]);
     }
 
     public function get ()
     {
-
+        return $this->response->statusOk(Ingredient::query()->orderByDesc('id')->cursorPaginate());
     }
 
     public function getRequired(GetRequiredIngredients $request)
