0118dcc21dccf0712ec0e6b9f3d24f82e4ee1ba2
Jad <jad.khoury14@gmail.com>
2021-06-12 17:13:39 +0300

    Added 'logic' to the 'add' method in 'Recipe' controller

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Ingredient;
+use App\Models\Recipe;
 use App\Base\BaseController;
-use App\Http\Requests\AddIngredientRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\AddRecipeRequest;
 
 class RecipeController extends BaseController
 {
-    public function add(AddIngredientRequest $request)
+    public function add(AddRecipeRequest $request)
     {
+        $data = $request->validated();
+        $ingredients = collect($data['ingredients'])->mapWithKeys(function ($ingredient) {
+            return [$ingredient['id'] => ['ingredient_amount' => $ingredient['amount']]];
+        });
 
+        DB::beginTransaction();
+        try {
+            $recipe = Recipe::query()->create($data);
+            $recipe->ingredients()->attach($ingredients);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error('Error creating recipe: ' .  $e->getMessage());
+            return $this->response->statusFail(['message' => 'Recipe Could not be created']);
+        }
+        DB::commit();
+
+        Ingredient::addAppendAttributes(['amount']);
+        $recipe->load('ingredients');
+        return $this->response->statusOk([
+            'message' => 'Recipe Created Successfully',
+            'recipe'  => $recipe
+        ]);
     }
 
     public function get()
