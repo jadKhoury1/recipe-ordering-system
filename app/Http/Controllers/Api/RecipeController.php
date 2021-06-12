c7e6cb8929e05de50630c606af5ab66a87a57d7a
Jad <jad.khoury14@gmail.com>
2021-06-12 12:41:14 +0300

    Created 'Recipe' controller

---
+++ app/Http/Controllers/Api/RecipeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Base\BaseController;
+use App\Http\Requests\AddIngredientRequest;
+
+class RecipeController extends BaseController
+{
+    public function add(AddIngredientRequest $request)
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+}
