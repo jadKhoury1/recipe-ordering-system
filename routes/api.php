fbb74c70ea40fdc11ce8a1abe52ab7c3e0c7121d
Jad <jad.khoury14@gmail.com>
2021-06-12 12:57:29 +0300

    Defined API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BoxController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\IngredientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::post('/ingredient', [IngredientController::class, 'add']);
+Route::get('/ingredients', [IngredientController::class, 'get']);
+Route::get('/requiredIngredients', [IngredientController::class, 'getRequired']);
+
+Route::post('/recipe', [RecipeController::class, 'add']);
+Route::get('/recipes', [RecipeController::class, 'get']);
+
+Route::post('/box', [BoxController::class, 'add']);
+
+
