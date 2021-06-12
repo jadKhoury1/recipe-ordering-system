0fc2af66b53d955500bf7f7df2f8e41c77edd52e
Jad <jad.khoury14@gmail.com>
2021-06-12 12:39:17 +0300

    Created 'Ingredient' controller

---
+++ app/Http/Controllers/Api/IngredientController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Base\BaseController;
+use App\Http\Requests\AddIngredientRequest;
+use App\Http\Requests\GetRequiredIngredients;
+
+class IngredientController extends BaseController
+{
+
+    public function add(AddIngredientRequest $request)
+    {
+
+    }
+
+    public function get ()
+    {
+
+    }
+
+    public function getRequired(GetRequiredIngredients $request)
+    {
+        
+    }
+}
