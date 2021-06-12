f866a16f73fcd90bc7d2c53bf2d2f9599f0d14ce
Jad <jad.khoury14@gmail.com>
2021-06-12 08:00:44 +0300

    Created 'Box' model

---
+++ app/Models/Box.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Base\BaseModel;
+
+class Box extends BaseModel
+{
+    /**
+     * Set the fields that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = ['order_date'];
+
+
+    /**
+     * Get the recipes associated with the box
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function recipes()
+    {
+        return $this->belongsToMany(
+            Recipe::class, 'box_recipes', 'box_id', 'recipe_id'
+        );
+    }
+}
