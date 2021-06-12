491fe96dd13a9282d31177133ac42c83c9260164
Jad <jad.khoury14@gmail.com>
2021-06-12 12:43:45 +0300

    Created 'Box' controller

---
+++ app/Http/Controllers/Api/BoxController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Base\BaseController;
+use App\Http\Requests\AddBoxRequest;
+
+class BoxController extends BaseController
+{
+    public function add(AddBoxRequest $request)
+    {
+
+    }
+}
